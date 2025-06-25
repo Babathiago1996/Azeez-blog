b77c323bcb34ceea7922e65f1674943644511d8f
Babathiago1996 <adebisiazeez1997@gmail.com>
2025-06-25 07:40:35 -0700

    make it responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className="h-[100px] bg-slate-800 text-white px-4 py-3 shadow-md ">
-      <div className="flex justify-between items-center w-[1000px] mx-auto  ">
-        <h1 className="text-[50px] text-red-500 ">Azeez Blog</h1>
-        <div className="flex gap-10 items-center text-[30px]">
-          <Link to="/" className=" hover:text-blue-500 focus:text-red-500">
+      <div className="flex justify-between items-center mx-auto  ">
+        <h1 className="text-[30px] md:text-[50px] text-red-500 ">Azeez Blog</h1>
+        <div className="flex gap-5 md:gap-10 items-center text-[30px]">
+          <Link to="/" className="text-white hover:text-blue-500 focus:text-red-500">
             Home
           </Link>
           <Link
